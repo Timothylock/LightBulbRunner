4d5d154d89184827bcf93cd229aa9120d9dcb00c
hmniay <Daphne@Daphnes-MacBook-Air.local>
2017-09-16 14:23:41 -0400

    player trigger box

--- LightBulbRunner/Assets/Scripts/PathGenerator.cs
+++ LightBulbRunner/Assets/Scripts/PathGenerator.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class PathGenerator : MonoBehaviour {
 
+	public GameObject Player;
+	public GameObject Path_I;
+	public int sightRange;
+
+	private Vector3 currSpawn;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		
 	}
+
+	void OnCollisionEnter (Collision other) {
+		//if (other.gameObject.CompareTag("") {
+			// do something
+		//}
+	}
+
+	void OnCollisionExit (Collision other) {
+		//if (other.gameObject.CompareTag("") {
+			// do something
+		//}
+	}
+
+
+
+
+	// How long is the path going down?
+	void GenerateLength () {
+		//pathLength = Random.Range (minPathLength, maxPathLength);
+
+		currSpawn = Path_I.GetComponent<PathBlock> ().spawnPoints [0].position;
+
+		GameObject p = Instantiate (Path_I, currSpawn, Quaternion.identity) as GameObject;
+
+		currSpawn = p.GetComponent<PathBlock> ().spawnPoints [0].position;
+
+
+	}
+
 }
